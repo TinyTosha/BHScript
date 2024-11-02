eec303e065e5da28944e5ef0838dc04d8ae4d34e
TinyTosha <159160965+TinyTosha@users.noreply.github.com>
2024-11-02 12:14:23 +0300

    Update bh.lua

--- bh.lua
+++ bh.lua
@@ -90,8 +90,21 @@ Tab:AddTextbox({
 	Name = "Go to Player",
 	Default = "",
 	TextDisappear = false,
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace[name]
+	Callback = function(name)
+		local plr = game:service"Players".LocalPlayer;
+		local tween_s = game:service"TweenService";
+		local info = TweenInfo.new(5,Enum.EasingStyle.Quad); --Everytime I tried going under 5, I got kicked/detected
+		function tp(...)
+   			local tic_k = tick();
+   			local params = {...};
+   			local cframe = CFrame.new(params[1],params[2],params[3]);
+   			local tween,err = pcall(function()
+       				local tween = tween_s:Create(plr.Character["HumanoidRootPart"],info,{CFrame=cframe});
+       				tween:Play();
+   		end)
+   			if not tween then return err end
+		end
+		tp(workspace.name)
 	end	  
 })
 })
