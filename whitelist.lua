1a1d3eff4b76169917780fdcfc582955234dc6ff
TinyTosha <159160965+TinyTosha@users.noreply.github.com>
2024-11-04 01:18:56 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,6 +1,12 @@
 getgenv().whitlist = {
-  load = {
-    ["p1"] = "Arseny2501_NOTFAKE"
-    ["p2"] = "Tiny2Tosha"
+  Load = {
+    ["pA"] = "Arseny2501_NOTFAKE",
+    ["pB"] = "Tiny2Tosha",
   }
 }
+
+if getgenv().whitlist.Load.pA  == LocalPlrName then
+elseif getgenv().whitlist.Load.pB  == LocalPlrName then
+else
+    game.Players.LocalPlayer:Kick(LocalPlrName .." is not on whitelist!")
+end
